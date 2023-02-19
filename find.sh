14aa7034a59bc7c83b2caf73bb9f838f6e95cd42
Edo2001Hakobyan <125294848+Edo2001Hakobyan@users.noreply.github.com>
2023-02-19 23:11:33 +0400

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-
-function search_files() {
-  for item in $(ls "$1"); do
+#Doing check that $1 is exist or not.	
+if [ -e $1 ]
+ then
+   function search_files() {
+      for item in $(ls "$1"); do
 #Doing check that $1 is directory.	  
-    if [ -d "$1/$item" ]; then	   
-	echo "./$item"    
-      search_files "$1/$item"
+         if [ -d "$1/$item" ]; then	   
+	      echo "./$item"    
+             search_files "$1/$item"
 #Doing check that $1 is file and equal all positional arguments.      
-    elif [[ "$1" == $*  ]]; then
-      echo "./$item"
-    fi
-  done
+         elif [[ "$1" == $*  ]]; then
+            echo "./$item"
+       fi
+      done
+else
+    echo "file or directory does not exist"
+fi  
 }
 
 
